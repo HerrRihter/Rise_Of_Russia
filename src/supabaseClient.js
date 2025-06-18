4cfb1e90e338dae365f95ac26ef4015d31bd4c4e
Rihter <herrrihter@gmail.com>
2025-06-19 01:33:09 +0700

    Configure secrets for GitHub Actions

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,11 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-// Используем уникальные плейсхолдеры
-const supabaseUrl = '__VITE_SUPABASE_URL__';
-const supabaseAnonKey = '__VITE_SUPABASE_ANON_KEY__';
-
-// Убираем проверку, так как она будет срабатывать во время сборки
-// if (!supabaseUrl || !supabaseAnonKey) { ... }
+const supabaseUrl = 'https://hlyajfpmuauowxbvnxxw.supabase.co'; // Вставьте ваш Project URL сюда
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhseWFqZnBtdWF1b3d4YnZueHh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxNTcwNjgsImV4cCI6MjA2NTczMzA2OH0.KW_WT0mNeKv49HCQxLK0KMRBPakVk-cPTQfOBw6GbhQ'; // Вставьте ваш anon public ключ сюда
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
